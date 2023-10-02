c77971f70888e1be2f7139feb824fcdc1f9a90d3
C42xe2 <erikdonath2006@gmail.com>
2023-10-02 21:31:01 +0200

    Headless works
    Headless works now.

--- Common/src/Triangle.cpp
+++ Common/src/Triangle.cpp
@@ -9,17 +9,34 @@ std::vector<std::vector<u32>> createTriangle(u32 n) {
 
 	for (u32 row = 1; row <= n; row++) {
 		std::vector<u32> current;
-
 		for (u32 i = 0; i <= row; i++) {
 			u32 v1 = ((i32)i - 1 >= 0) ? tri[row - 1][i - 1] : 0;
 			u32 v2 = ((i32)i < row)    ? tri[row - 1][i + 0] : 0;
 			current.push_back(v1 + v2);
 		}
-
 		tri.push_back(current);
 	}
 	return tri;
 }
 std::string createEquation(std::vector<u32> v) {
+	std::stringstream ss;
+	u32 n = v.size() - 1;
+	ss << "(a+b)^" << n << "'=";
 
+	for (u32 i = 0; i <= n; i++) {
+		if (v[i] > 1) ss << v[i];
+
+		u32 aExponent = n - i;
+		if (aExponent == 1) ss << "a";
+		if (aExponent >  1) ss << "a^" << aExponent << "'";
+
+		u32 bExponent = i;
+		if (bExponent == 1) ss << "b";
+		if (bExponent >  1) ss << "b^" << bExponent << "'";
+
+		if (i != n) ss << "+";
+	}
+
+	ss << '.'; // End
+	return ss.str();
 }
---
+++ Headless/src/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "Defines.hpp"
+#include "Triangle.hpp"
+
+i32 main(i32 argc, char* argsv[]) {
+	u32 n = 2;
+	std::cout << "Gebe die Hochzahl an: ";
+	std::cin >> n;
+	std::vector<std::vector<u32>> tri = createTriangle(n);
+
+	if (n < 255) {
+		std::cout << "Das Pascalsche Dreieck: " << std::endl;
+		for (std::vector<u32> row : tri) {
+			for (u32 value : row)
+				std::cout << value << " ";
+			std::cout << std::endl;
+		}
+	}
+	else std::cout << "Das Pascalsche Dreieck wird nicht angezeigt." << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Die daraus resultierende Formel: " << std::endl;
+	std::cout << createEquation(tri.back()) << std::endl;
+}
