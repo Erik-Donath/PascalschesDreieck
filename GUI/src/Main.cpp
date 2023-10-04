5d5e7ef1140bbbb2a1c3f86b2ad7fddd17d5cfe5
C42xe2 <erikdonath2006@gmail.com>
2023-10-04 07:56:58 +0200

    Windowing for GUI version.
    Added raylib includes and implemented a basic Window.

--- GUI/src/Main.cpp
+++ GUI/src/Main.cpp
@@ -1,6 +1,17 @@
+#include <raylib.h>
+#include <raymath.h>
+
 #include "Defines.hpp"
 #include "Triangle.hpp"
 
 i32 main(i32 argc, char* argsv[]) {
-	
+	InitWindow(800, 600, "Pascalsches Dreieck");
+
+	while(!WindowShouldClose()) {
+		BeginDrawing(); {
+			ClearBackground(BLACK);
+		} EndDrawing();
+	}
+
+	CloseWindow();
 }
